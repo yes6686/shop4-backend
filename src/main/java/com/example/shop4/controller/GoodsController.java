b031f0c73e9b98b76182ef7022f656fee25f0ca1
안예찬 <yes490411@gmail.com>
2024-08-13 19:54:46 +0900

    postmapping까지 만듬

---
+++ src/main/java/com/example/shop4/controller/GoodsController.java
@@ -0,0 +1,23 @@
+package com.example.shop4.controller;
+
+
+import com.example.shop4.dto.GoodsDto;
+import com.example.shop4.service.GoodsService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/Goods")
+public class GoodsController {
+    private GoodsService goodsService;
+
+    @PostMapping
+    public ResponseEntity<GoodsDto> createGoods(@RequestBody GoodsDto goodsDto) {
+        GoodsDto savedGoods = goodsService.createGoods(goodsDto);
+        return new ResponseEntity<>(savedGoods, HttpStatus.CREATED);
+    }
+}
