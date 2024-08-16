259ea4196f24fe8e563561d995cb5ba456f76a8f
kimdonghyuk <kdh367036@naver.com>
2024-08-16 16:24:39 +0900

    addCartEntity

---
+++ src/main/java/com/example/shop4/entity/Cart.java
@@ -0,0 +1,26 @@
+package com.example.shop4.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "goods_id")
+    private Goods goods;
+    private long quantity;
+
+}
