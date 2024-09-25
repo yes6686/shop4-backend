a99cabf871cbbb03bb8da5d260b95ad5efa9e4a8
ok63477 <ok63477@gmail.com>
2024-09-25 19:47:37 +0900

    memberId와 goodsId로 장바구니 검색하는 기능을 위해 추가

--- src/main/java/com/example/shop4/repository/CartRepository.java
+++ src/main/java/com/example/shop4/repository/CartRepository.java
@@ -13,6 +13,7 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
     Cart findByMemberAndGoods(Member member, Goods goods);
     List<Cart> findByMemberId(Long memberId); // 특정 회원의 장바구니 상품 검색
     void deleteByGoodsId(Long goodsId); // goodsId로 삭제하는 메서드
+    Cart findByMemberIdAndGoodsId(Long memberId,Long goodsId); //멤버아이디와 상품아이디로 있는지 찾기
 }
 
 
